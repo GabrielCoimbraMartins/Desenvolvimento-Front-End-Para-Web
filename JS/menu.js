7ce300c61de1110f0792d264dc349ac90f69d64f
GabrielCoimbraMartins <gabriel.coimbramartins@gmail.com>
2025-11-02 11:51:37 -0300

    v0.3 - atualizado

---
+++ JS/menu.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.js-hamburger');
+const mobileNav = document.querySelector('.js-mobile-nav');
+
+hamburger.addEventListener('click', () => {
+    mobileNav.classList.toggle('open');
+});
